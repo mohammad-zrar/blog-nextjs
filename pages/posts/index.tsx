184e4e40326be9bdf9b6af8ca843197d0cf0a607
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-10-29 20:01:09 +0300

    Working On The Post Detail Page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,3 +1,36 @@
+import AllPosts from "@/components/posts/all-posts";
+import type { Post } from "@/types";
+
+const DUMMY_POSTS: Post[] = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started With Next.js',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'Next.js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2024-10-29'
+    },
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started With Next.js',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'Next.js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2024-10-29'
+    },
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started With Next.js',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'Next.js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2024-10-29'
+    },
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started With Next.js',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'Next.js is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+        date: '2024-10-29'
+    },
+];
 export default function AllPostspage() {
-    return <h1>All Posts Page</h1>
+    return <AllPosts posts={DUMMY_POSTS} />
 }
