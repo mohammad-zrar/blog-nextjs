43d2d9897264cb3215a5813e77efea2349aa2bc8
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-01 22:36:37 +0300

    feat(components\contact\contact-form.tsx): Add UI feedback with notification

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,27 +1,95 @@
-import { useState, FormEvent } from 'react';
+import { useState, FormEvent, useEffect } from 'react';
+
 import classes from './contact-form.module.css';
+import Notification from '../ui/notification';
+
+async function sendContactData(contactDetails: any) {
+    const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(contactDetails),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong')
+    }
+}
 
 export default function ContactForm() {
     const [enteredEmail, setEnteredEmail] = useState('')
     const [enteredName, setEnteredName] = useState('')
     const [enteredMessage, setEnteredMessage] = useState('')
+    const [requestStatus, setRequestStatus] = useState<string>() // 'pending', 'success', 'error'
+    const [requestError, setRequestError] = useState<string>();
 
-    function sendMessagehandler(event: FormEvent<HTMLFormElement>) {
+    useEffect(() => {
+
+        if (requestStatus === 'success' || requestStatus === 'error') {
+            const timer = setTimeout(() => {
+                setRequestStatus('');
+                setRequestError('');
+            }, 3000);
+
+            return () => clearTimeout(timer)
+        }
+    }, [requestStatus]);
+
+    async function sendMessagehandler(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         // optional: add client-side validation
 
-        fetch('/api/contact', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: enteredEmail,
-                name: enteredName,
-                message: enteredMessage
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        setRequestStatus('pending');
+
+        try {
+            await sendContactData(
+                {
+                    email: enteredEmail,
+                    name: enteredName,
+                    message: enteredMessage
+                }
+            )
+            setRequestStatus('success');
+            setEnteredEmail('');
+            setEnteredName('');
+            setEnteredMessage('');
+
+        } catch (err) {
+            const errorMessage = (err as Error).message || 'An unknown error occurred';
+            setRequestError(errorMessage);
+            setRequestStatus('error');
+        }
+
+    }
+
+    let notification;
+
+    if (requestStatus === 'pending') {
+        notification = {
+            status: 'pending',
+            title: 'Sending message...',
+            message: 'Your message is on its way!'
+        }
+    }
+
+    if (requestStatus === 'success') {
+        notification = {
+            status: 'success',
+            title: 'Success!',
+            message: 'Message sent successfully!'
+        }
+    }
+
+    if (requestStatus === 'error') {
+        notification = {
+            status: 'error',
+            title: 'Error!',
+            message: requestError
+        }
     }
 
     return <section className={classes.contact}>
@@ -45,5 +113,6 @@ export default function ContactForm() {
                 <button>Send Message</button>
             </div>
         </form>
+        {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
     </section>
 }
--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,7 @@
+import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'POST') {
         const {email, name, message} = req.body;
 
@@ -11,13 +12,35 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         }
 
         // Store it in a database
-        const newMessage = {
+        const newMessage: any = {
             email,
             name, 
             message
         }
 
-        console.log(newMessage);
+        let client;
+
+        try{
+             client = await MongoClient.connect('mongodb://localhost:27017/')
+        } catch(err) {
+
+            res.status(500).json({message: 'Internal server error'})
+            return;
+        }
+        
+        const db = client.db('blog-nextjs');
+
+        try{
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch(err) {
+            client.close();
+            res.status(500).json({message: 'Storing message failed'})
+            return;
+        }
+        
+
+        client.close();
 
         res.status(201).json({message: 'Successfully stored message!', data: newMessage})
     }
